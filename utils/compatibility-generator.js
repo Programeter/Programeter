9528c1390b3a0298408bc88a57a6be249e612db7
Joseph McArdle <slater.mcardle@outlook.com>
2022-03-19 13:54:13 -0700

    time-save commit

--- utils/compatibility-generator.js
+++ utils/compatibility-generator.js
@@ -1,19 +1,25 @@
 const questions = require('questions');
-const { Question, Category } = require('../models')
+const { Answer, Question, Category, Language } = require('../models');
 
 const generateCompatibility = async (user1Id, user2Id) => {
-    const questions = Question.findAll({
-        include: [
-            {
-                model: 'category',
-                attributes: [
-                    'id',
-                    'name',
-                    'weight'
-                ]
-            }
-        ]
+    const questions =await Question.findAll();
+
+    const user1Answers = await Answer.findAll({
+        where: {
+            user_id: user1Id,
+        },
+        order: ['question_id', 'ASC'],
     });
+
+    const user2Answers = await Answer.findAll({
+        where: {
+            user_id: user2Id,
+        },
+        order: ['question_id', 'ASC'],
+    });
+
+
+
     // Variable to track compatibility percentage. Starts at 100 and deducts
     let compatibility_percentage = 100;
     // Question 1: Tabs or spaces??
