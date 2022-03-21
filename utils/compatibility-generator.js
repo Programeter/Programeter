17148651e87a54f28ba91dbf5c8c8c59ed4e8c0b
Joseph McArdle <slater.mcardle@outlook.com>
2022-03-21 10:18:25 -0700

    Compatibility generator alpha version

--- utils/compatibility-generator.js
+++ utils/compatibility-generator.js
@@ -2,8 +2,12 @@ const questions = require('questions');
 const { Answer, Question, Category, Language } = require('../models');
 
 const generateCompatibility = async (user1Id, user2Id) => {
-    const questions =await Question.findAll();
+    const questions = await Question.findAll();
+    const categories = await Category.findAll();
 
+    // Variable to track compatibility percentage. Starts at 100 and deducts
+    let compatibility_percentage = 100;
+    
     const user1Answers = await Answer.findAll({
         where: {
             user_id: user1Id,
@@ -18,38 +22,10 @@ const generateCompatibility = async (user1Id, user2Id) => {
         order: ['question_id', 'ASC'],
     });
 
+    user1Answers.forEach((answer, index) => {
+        const question = questions.find(x => x.id === answer.answers_id);
+        const weight = categories.find(x => x.id === question.category_id);
 
-
-    // Variable to track compatibility percentage. Starts at 100 and deducts
-    let compatibility_percentage = 100;
-    // Question 1: Tabs or spaces??
-    if (user1Answers.answer1 != user2Answers.answer1) { 
-        compatibility_percentage -= questions.question1.weight;
-    }
-
-    // Question 2: Do you prioritize clean, reliable code or a functioning product?
-    if (user1Answers.answer2 != user2Answers.answer2) {
-        compatibility_percentage -= questions.question2.weight;
-    }
-
-    // Question 3: How do you prefer to test your code?
-    if (user1Answers.answer3 != user2Answers.answer3) {
-        compatibility_percentage -= questions.question3.weight;
-    }
-
-    // Question 4: On a scale of 1 to 5, how much do you comment?
-    compatibility_percentage -= Math.abs(user1Answers.answer4 - user2Answers.answer4) * questions.question4.weight;
-
-    // Question 5: What time of day do you work best? (values are between 0 and 2)
-    compatibility_percentage -= Math.abs(user1Answers.answer5 - user2Answers.answer5) * questions.question5.weight;
-
-    // Question 6: How do you prefer to receive criticism?
-    if (user1Answers.answer6 != user2Answers.answer6) {
-        compatibility_percentage -= questions.question6.weight;
-    }
-
-    // Question 7: Do you listen to music while you code?
-    if (user1Answers.answer7 != user2Answers.answer7) {
-        compatibility_percentage -= questions.question7.weight;
-    }
+        compatibility_percentage -= Math.abs(answer - user2Answers[index]) * weight;
+    });
 };
