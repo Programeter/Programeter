b4dea89b49af27a55ca7ee746ff9ff1f90d60e60
Joseph McArdle <slater.mcardle@outlook.com>
2022-03-22 18:18:04 -0700

    compatibility algorithm as far as it can go without models and routes. resume generator started

--- utils/compatibility-generator.js
+++ utils/compatibility-generator.js
@@ -1,22 +1,20 @@
 const questions = require('questions');
-const { Answer, Question, Category, Language } = require('../models');
+const { Answer, Question, Language } = require('../models');
 
-const generateCompatibility = async (user1Id, user2Id) => {
-    const questions = await Question.findAll();
-    const categories = await Category.findAll();
-
+const generateCompatibility = async (user1Id, user2Id, searchLanguages) => {
     // Variable to track compatibility percentage. Starts at 100 and deducts
-    let compatibility_percentage = 100;
-    
+    let personal_compatibility_percentage = 100;
+    let work_compatibility_percentage = 100;
+    let language_compatibility_percentage = 100;
+    let personalQuestionWeight = 100 / await Question.count({ where: {'category' : 'personal'}});
+    let workQuestionWeight = 100 / await Question.count({ where: {'category' : 'work'}});
+
     const user1Answers = await Answer.findAll({
         where: {
             user_id: user1Id,
         },
         include: [{
-            model: 'question',
-            include: [{
-                model: 'category'
-            }]
+            model: 'question'
         }],
         order: ['question_id', 'ASC'],
     });
@@ -25,6 +23,9 @@ const generateCompatibility = async (user1Id, user2Id) => {
         where: {
             user_id: user2Id,
         },
+        include: [{
+            model: 'question'
+        }],
         order: ['question_id', 'ASC'],
     });
 
@@ -32,6 +33,28 @@ const generateCompatibility = async (user1Id, user2Id) => {
         // const question = questions.find(x => x.id === answer.answers_id);
         // const weight = categories.find(x => x.id === question.category_id);
 
-        compatibility_percentage -= Math.abs(answer - user2Answers[index]) * answer.question.category.weight;
+        if (answer.question.category == 'personal' && answer != user2Answers[index]) {
+            personal_compatibility_percentage -= personalQuestionWeight;
+        } else if (answer.question.category == 'work' && answer != user2Answers[index]) {
+            work_compatibility_percentage -= workQuestionWeight;
+        }
     });
+
+    const user2Languages = await User.findByPk(user2Id, {
+        include: ['language']
+    });
+
+    const languageWeight = searchLanguages.length;
+
+    searchLanguages.forEach( language => {
+        if (! user2Languages.search(userLanguage => {language == userLanguage})) {
+            language_compatibility_percentage -= languageWeight;
+        }
+    });
+    
+    return {
+        'personal_compatibility': personal_compatibility_percentage,
+        'work_compatibility': work_compatibility_percentage,
+        'language_compataibility': language_compatibility_percentage
+    };
 };
