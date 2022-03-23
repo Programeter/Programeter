3797b97178dcc04ed5a27bbac247ec454a5d11ef
Joseph McArdle <slater.mcardle@outlook.com>
2022-03-22 21:01:37 -0700

    created language and language link models and added file to query github languages

---
+++ models/Language.js
@@ -0,0 +1,29 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Language extends Model {}
+
+Language.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        language_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "language",
+    },
+);
+
+module.exports = Language;
--- models/answers.js
+++ models/answers.js
@@ -11,22 +11,21 @@ answers.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    answerType1: {
+    answerText: {
       type: DataTypes.STRING,
     },
-    answerType2: {
+    answerValue: {
       type: DataTypes.INTEGER,
     },
-    answerType3: {
-      type: DataTypes.BOOLEAN,
-    },
-    user_id: {
-      type: DataTypes.INTEGER,
+    question_id: {
+      type: DataTypes.INTEGER, 
+      allowNull: false,
       references: {
-        model: 'user',
+        model: 'question',
         key: 'id',
-      },
-    },
+        unique: false,
+      }
+    }
   },
   {
     sequelize,
