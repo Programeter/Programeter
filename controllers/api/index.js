be304814d526562f614d313a549d772b9561afca
Roberto Gonzalez <rgonzalez7359@gmail.com>
2022-03-24 16:16:10 -0700

    added language route to api indexjs

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const userRoute = require('./user-routes');
 const questionRoute = require('./question-routes');
+const languageRoute = require('./language-routes')
 
 router.use('/user', userRoute);
 router.use('/questions', questionRoute);
+router.use('/language', languageRoute);
 
 module.exports = router;
