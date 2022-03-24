c4efe1ba414b92c5f8a9b513943487ad048a3334
Roberto Gonzalez <rgonzalez7359@gmail.com>
2022-03-24 16:40:38 -0700

    added async to id get, created For loop for users

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const { User, Language, LanguageLink, Option, UserAnswer } = require('../../models')
 
 // GET one user
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   try {
     const userData = await User.findByPk(req.params.id, {
       include: [
-        {model: Language, through: LanguageLink, as: 'known_languages'},
+        {model: Language, through: LanguageLink, as: 'user_languages'},
         {model: Option, through: UserAnswer, as: 'user_answers'}
       ]
     });
@@ -27,7 +27,22 @@ router.post('/', async (req, res) => {
         username: req.body.username,
         email: req.body.email,
         password: req.body.password,
+        github_name: req.body.github_name
       });
+
+      // for loop here
+      req.body.answers.forEach((answer)=> {
+        const newUserAnswer = await UserAnswer.create({
+          user_id: dbUserData.id,
+          option_id: answer
+        })
+      })
+      req.body.languages.forEach((language)=> {
+        const newLanguageLink = await LanguageLink.create({
+          user_id: dbUserData.id,
+          language_id: language
+        })
+      })
   
       req.session.save(() => {
         req.session.loggedIn = true;
@@ -39,6 +54,27 @@ router.post('/', async (req, res) => {
       res.status(500).json(err);
     }
   });
+
+  // DELETE a user
+router.delete('/:id', async (req, res) => {
+  try {
+    const userData = await User.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!userData) {
+      res.status(404).json({ message: 'No user found with that id!' });
+      return;
+    }
+
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 
   // Login
 router.post('/login', async (req, res) => {
