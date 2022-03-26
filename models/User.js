0bfc94d0abcb8c32e935ad78b317d02904b60413
Slater McArdle <slater.mcardle@outlook.com>
2022-03-25 21:11:15 -0700

    fixed user model again

--- models/User.js
+++ models/User.js
@@ -35,11 +35,10 @@ User.init(
     email: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true/* ,
+      unique: true,
       validate: {
         isEmail: true,
-        },
-      }, */
+      },
     },
   },
   {
