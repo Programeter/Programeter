7e7b197d06723c5587ba692752b107d7d1187682
Roberto Gonzalez <rgonzalez7359@gmail.com>
2022-03-26 11:36:17 -0700

    puts input value in search bar as a cookie

---
+++ public/js/searchcookie.js
@@ -0,0 +1,39 @@
+var searchInput = document.getElementById('searchbar');
+var searchBtn = document.getElementById('searchBtn');
+// var searchValue = document.getElementById()
+
+searchBtn.addEventListener("click", ()=> {
+    setCookie("search cookie", searchInput.value, 30)
+    console.log(searchInput.value)
+})
+
+
+function setCookie(name,value,exp_days) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exp_days*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+function getCookie(name) {
+    var cname = name + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++){
+        var c = ca[i];
+        while(c.charAt(0) == ' '){
+            c = c.substring(1);
+        }
+        if(c.indexOf(cname) == 0){
+            return c.substring(cname.length, c.length);
+        }
+    }
+    return "";
+}
+
+function deleteCookie(name) {
+    var d = new Date();
+    d.setTime(d.getTime() - (60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = name + "=;" + expires + ";path=/";
+}
