e24f37ed0b388962fd13c869e25f2eebbc80fd2c
Roberto Gonzalez <rgonzalez7359@gmail.com>
2022-03-28 19:57:46 -0700

    added darkmode functionality to login page

---
+++ public/js/styling.js
@@ -0,0 +1,8 @@
+let form = document.getElementById('form1')
+
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    form.classList.toggle("dark-mode")
+  }
