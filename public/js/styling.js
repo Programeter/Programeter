9b571a7a93d3cee902531bd14d1f46042dbf0f14
Roberto Gonzalez <rgonzalez7359@gmail.com>
2022-03-29 17:11:53 -0700

    keeps dark mode on when clicking button

--- public/js/styling.js
+++ public/js/styling.js
@@ -1,5 +1,14 @@
 let form = document.getElementById('form1');
+let themeSwitcher = document.getElementById('themeSwitcher');
 
+
+function init(){
+  if(document.cookie && document.cookie==="isDark=true"){
+    // clicks the button to dark mode side when dark mode is left on
+  themeSwitcher.click()
+  }
+  
+}
 
 function myFunction() {
     var element = document.body;
@@ -7,4 +16,13 @@ function myFunction() {
     if (form != null) {
       form.classList.toggle("dark-mode");
     }
+    // sets cookie to be either either on dark mode or off
+    if(element.classList.contains('dark-mode')){
+      document.cookie = "isDark=true"
+    } else {
+      document.cookie = "isDark=false"
+    }
+    
   }
+
+  init();
